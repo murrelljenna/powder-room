fd30d295a487144ce5e60c0748ff12918c3303fe
Jenna Murrell <murrelljenna@gmail.com>
2024-11-20 16:17:14 -0500

    Toggle toxic spray on and off

--- spray_gun.gd
+++ spray_gun.gd
@@ -1,9 +1,20 @@
 extends Node3D
 
 		
+var firing := false
+@export var particles_node: GPUParticles3D
+
 func _fire() -> void:
+	firing = true
+	particles_node.emitting = true
 	print("fire")
 
+func _stop_firing() -> void:
+	firing = false
+	particles_node.emitting = false
+
 func _unhandled_input(event):
-	if Input.is_action_just_pressed("fire"):
+	if Input.is_action_pressed("fire"):
 		_fire()
+	if Input.is_action_just_released("fire"):
+		_stop_firing()

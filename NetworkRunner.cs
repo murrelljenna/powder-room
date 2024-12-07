6926502471bed0a5433c0060d1b718d9b554d1fd
Jenna Murrell <murrelljenna@gmail.com>
2024-12-07 13:03:17 -0500

    Add basic server code

--- NetworkRunner.cs
+++ NetworkRunner.cs
@@ -1,13 +1,15 @@
 using Godot;
 using System;
-using powdered_networking;
+//using powdered_networking;
 
-public partial class StarterScene : Node3D
+public partial class NetworkRunner : Node
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		powdered_networking.Server.StartServer();
+		GD.Print("Before");
+		Server.StartServer();
+		GD.Print("After");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
